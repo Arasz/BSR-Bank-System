34e58343fe6b374b23f1f248abdd182d1f66423e
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-12-04 17:26:15 +0100

    Name service and add nuget packages for AF and EF

---
+++ Account-Mangament-Service/AccountMangmentService/IAccountManagmentService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace AccountMangmentService
+{
+    /// <summary>
+    /// Responsible for user bank account management 
+    /// </summary>
+    [ServiceContract]
+    public interface IAccountManagmentService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
