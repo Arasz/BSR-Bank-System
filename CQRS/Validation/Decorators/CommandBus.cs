71545fa129815b485536d96662067bb193f77c97
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-12-11 17:19:10 +0100

    Add validation decorator implementation

---
+++ CQRS/Validation/Decorators/CommandBus.cs
@@ -0,0 +1,29 @@
+﻿using CQRS.Commands;
+using System;
+
+namespace CQRS.Validation.Decorators
+{
+    /// <summary>
+    /// Command bus decorator. Validates command before send. 
+    /// </summary>
+    public class CommandBus : ICommandBus
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly Func<Type, IValidation> _validationFactory;
+
+        public CommandBus(ICommandBus commandBus, Func<Type, IValidation> validationFactory)
+        {
+            _commandBus = commandBus;
+            _validationFactory = validationFactory;
+        }
+
+        public void Send<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            var validator = _validationFactory(typeof(TCommand)) as IValidation<TCommand>;
+
+            validator?.Validate(command);
+
+            _commandBus.Send(command);
+        }
+    }
+}
---
+++ CQRS/Validation/Decorators/QueryBus.cs
@@ -0,0 +1,29 @@
+﻿using CQRS.Queries;
+using System;
+
+namespace CQRS.Validation.Decorators
+{
+    /// <summary>
+    /// Query bus decorator. Validates query before send. 
+    /// </summary>
+    public class QueryBus : IQueryBus
+    {
+        private readonly IQueryBus _queryBus;
+        private readonly Func<Type, IValidation> _validationFactory;
+
+        public QueryBus(IQueryBus queryBus, Func<Type, IValidation> validationFactory)
+        {
+            _queryBus = queryBus;
+            _validationFactory = validationFactory;
+        }
+
+        public TResult Send<TResult, TQuery>(TQuery query) where TQuery : IQuery<TResult>
+        {
+            var validator = _validationFactory(typeof(TQuery)) as IValidation<TQuery>;
+
+            validator?.Validate(query);
+
+            return _queryBus.Send<TResult, TQuery>(query);
+        }
+    }
+}
