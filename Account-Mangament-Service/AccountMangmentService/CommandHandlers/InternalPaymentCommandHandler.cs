429a772846ed6698585db8c10dc1ee8e44061755
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-12-14 23:40:23 +0100

    Add internal payment command handler

---
+++ Account-Mangament-Service/AccountMangmentService/CommandHandlers/InternalPaymentCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using AccountMangmentService.Commands;
+using CQRS.Commands;
+
+namespace AccountMangmentService.CommandHandlers
+{
+    public class InternalPaymentCommandHandler : ICommandHandler<InternalPaymentCommand>
+    {
+        public void HandleCommand(InternalPaymentCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Account-Mangament-Service/AccountMangmentService/Commands/MinimalPaymentCommand.cs
+++ Account-Mangament-Service/AccountMangmentService/Commands/MinimalPaymentCommand.cs
@@ -1,9 +1,11 @@
-﻿namespace AccountMangmentService.Commands
+﻿using CQRS.Commands;
+
+namespace AccountMangmentService.Commands
 {
     /// <summary>
     /// Minimal common part of all payment commands 
     /// </summary>
-    public abstract class MinimalPaymentCommand
+    public abstract class MinimalPaymentCommand : ICommand
     {
         public decimal Amount { get; set; }
     }
