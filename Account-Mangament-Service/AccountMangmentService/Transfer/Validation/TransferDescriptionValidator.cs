a464a6b7587db2d139ca8410f3591cbf32f6bc5a
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-12-17 19:25:26 +0100

    Add and complete transfer description validation and test

--- Account-Mangament-Service/AccountMangmentService/Transfer/Validation/TransferDescriptionValidator.cs
+++ Account-Mangament-Service/AccountMangmentService/Transfer/Validation/TransferDescriptionValidator.cs
@@ -1,46 +1,47 @@
 ﻿using AccountMangmentService.Transfer.Dto;
 using FluentValidation;
 using Shared.ChecksumCalculator;
+using System;
 
 namespace AccountMangmentService.Transfer.Validation
 {
     public class TransferDescriptionValidator : AbstractValidator<TransferDescription>
     {
+        private readonly Func<string, bool> _accountExistPredicate;
         private readonly IAccountChecksumCalculator _checksumCalculator;
 
-        public TransferDescriptionValidator(IAccountChecksumCalculator checksumCalculator)
+        public TransferDescriptionValidator(IAccountChecksumCalculator checksumCalculator, Func<string, bool> accountExistPredicate)
         {
             _checksumCalculator = checksumCalculator;
+            _accountExistPredicate = accountExistPredicate;
 
             CascadeMode = CascadeMode.StopOnFirstFailure;
 
             RuleFor(description => description.Amount)
-                .GreaterThan(0);
-            //regex https://regex101.com/r/qnQGLK/1
-            RuleFor(description => description.ReceiverAccount)
-                .NotEmpty()
-                .Length(26)
-                .Matches("^[0-9]{2}112241[0]{2}[0-9]{18}")
-                .WithErrorCode("22")
-                .Must(HaveCorrectChecksum)
-                .Must(ExistInTheBank);
+                .GreaterThanOrEqualTo(0);
 
+            //regex https://regex101.com/r/LEt65g/1
             RuleFor(description => description.SenderAccount)
                 .NotEmpty()
                 .Length(26)
-                .Matches("^[0-9]{2}[0-9]{4}[0]{2}[0-9]{18}")
-                .Must(HaveCorrectChecksum);
+                .Matches(@"^[0-9]{2}[0-9]{6}[0]{2}[0-9]{16}")
+                .Must(HaveCorrectChecksum)
+                .Must(ExistInTheBank)
+                .When(description => description.SenderAccount.Substring(2, 6).Equals("112241"), ApplyConditionTo.CurrentValidator)
+                .WithErrorCode("404 - Receiver account not found");
+
+            RuleFor(description => description.ReceiverAccount)
+                .NotEmpty()
+                .Length(26)
+                .Matches(@"^[0-9]{2}[0-9]{6}[0]{2}[0-9]{16}")
+                .Must(HaveCorrectChecksum)
+                .Must(ExistInTheBank)
+                .When(description => description.ReceiverAccount.Substring(2, 6).Equals("112241"), ApplyConditionTo.CurrentValidator)
+                .WithErrorCode("404 - Sender account not found");
         }
 
-        private bool ExistInTheBank(string accountNumber) => true;
+        private bool ExistInTheBank(string accountNumber) => _accountExistPredicate?.Invoke(accountNumber) ?? false;
 
-        private bool HaveCorrectChecksum(string accountNumber)
-        {
-            var checksumLength = 2;
-
-            var checksum = int.Parse(accountNumber.Substring(0, checksumLength));
-
-            return checksum == _checksumCalculator.Calculate(accountNumber.Substring(checksumLength, accountNumber.Length - checksumLength));
-        }
+        private bool HaveCorrectChecksum(string accountNumber) => _checksumCalculator.IsCorrect(accountNumber);
     }
 }
