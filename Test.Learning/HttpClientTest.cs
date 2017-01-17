1b6567a1363489172212382ed2b0ded5c1fc6261
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-17 22:36:18 +0100

    Add http client learning tests

---
+++ Test.Learning/HttpClientTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Test.Learning
+{
+    public class HttpClientTest
+    {
+        private string _getRequestUri = "http://www.brainjar.com/java/host/test.html";
+
+        [Fact]
+        public async Task MakeGetRequest_WithCorrectUri_GetSimpleWebPage()
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetAsync(_getRequestUri, HttpCompletionOption.ResponseContentRead);
+
+            httpResponseMessage.StatusCode
+                .Should()
+                .HaveFlag(HttpStatusCode.OK);
+
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            content.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task ReuseHttpClient_MakeTwoGetRequestWithOneClient_GetSimpleWebPageTwoTimes()
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetAsync(_getRequestUri, HttpCompletionOption.ResponseContentRead);
+
+            httpResponseMessage.StatusCode
+                .Should()
+                .HaveFlag(HttpStatusCode.OK);
+
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            content.Should().NotBeNullOrEmpty();
+
+            httpResponseMessage = await httpClient.GetAsync(_getRequestUri, HttpCompletionOption.ResponseContentRead);
+
+            httpResponseMessage.StatusCode
+                .Should()
+                .HaveFlag(HttpStatusCode.OK);
+
+            content = await httpResponseMessage.Content.ReadAsStringAsync();
+            content.Should().NotBeNullOrEmpty();
+        }
+    }
+}
