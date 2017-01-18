cf4fff30bb1b7398ce1c74f5895340420e7501bf
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-18 18:39:04 +0100

    Add custom authentiaction validator

--- Account-Mangament-Service/AccountMangmentService/Validation/CustomUserNamePasswordValidator.cs
+++ Account-Mangament-Service/AccountMangmentService/Validation/CustomUserNamePasswordValidator.cs
@@ -3,12 +3,24 @@ using System.IdentityModel.Tokens;
 using System.Linq;
 using System.ServiceModel;
 using Data.Core;
+using Shared.Security;
 
 namespace Service.Bank.Validation
 {
     public class CustomUserNamePasswordValidator : UserNamePasswordValidator
     {
-        private BankDataContext _dataContext = new BankDataContext();
+        private readonly IPasswordHasher _passwordHasher;
+        private BankDataContext _dataContext;
+
+        public CustomUserNamePasswordValidator() : this(new BankDataContext(), new DefaultPasswordHasher())
+        {
+        }
+
+        public CustomUserNamePasswordValidator(BankDataContext dataContext, IPasswordHasher passwordHasher)
+        {
+            _dataContext = dataContext;
+            _passwordHasher = passwordHasher;
+        }
 
         /// <exception cref="SecurityTokenException"> Correct user name and password required </exception>
         public override void Validate(string userName, string password)
@@ -18,7 +30,7 @@ namespace Service.Bank.Validation
 
             var authenticatedUser = _dataContext.Users.SingleOrDefault(user => user.Name == userName) ?? User.NullUser;
 
-            authenticatedUser.AuthenticateUser(userName, password);
+            authenticatedUser.AuthenticateUser(userName, password, _passwordHasher);
         }
     }
 }
