b4548220ebc4d70d676b289bfd5f66b75e72b96d
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-19 19:27:15 +0100

    Derive external transfer handler

--- Account-Mangament-Service/AccountMangmentService/CommandHandlers/External/ExternalTransferCommandHandler.cs
+++ Account-Mangament-Service/AccountMangmentService/CommandHandlers/External/ExternalTransferCommandHandler.cs
@@ -1,48 +1,55 @@
 ﻿using System.Linq;
 using CQRS.Commands;
 using Data.Core;
+using Service.Bank.CommandHandlers.Base;
 using Service.Bank.Commands;
+using Service.Bank.Operations;
 using Service.Contracts;
 using Service.Dto;
 
 namespace Service.Bank.CommandHandlers.External
 {
-    public class ExternalTransferCommandHandler : ICommandHandler<ExternalTransferCommand>
+    public class ExternalTransferCommandHandler : BankOperationCommandHandler<ExternalTransferCommand>
     {
         private readonly ICommandBus _commandBus;
         private readonly BankDataContext _dataContext;
         private readonly IInterbankTransferService _interbankTransferService;
 
-        public ExternalTransferCommandHandler(BankDataContext dataContext, IInterbankTransferService interbankTransferService, ICommandBus commandBus)
+        public ExternalTransferCommandHandler(BankDataContext dataContext, IInterbankTransferService interbankTransferService, ICommandBus commandBus, IOperationRegister operationRegister) : base(dataContext, operationRegister)
         {
             _dataContext = dataContext;
             _interbankTransferService = interbankTransferService;
             _commandBus = commandBus;
         }
 
-        public void HandleCommand(ExternalTransferCommand command)
+        public override void HandleCommand(ExternalTransferCommand command)
         {
-            MakeInterbankTransfer(command);
+            _transferDescription = command.TransferDescription;
 
-            UpdateAccountBalance(command);
+            MakeInterbankTransfer();
+
+            UpdateAccountBalance();
+
+            RegisterOperation();
 
             _commandBus.Send(new ExternalTransferChargeCommand(command.TransferDescription));
         }
 
-        private void MakeInterbankTransfer(ExternalTransferCommand command)
+        protected override void ChangeAccountBalance(decimal amount) => Account.Balance -= amount;
+
+        private void MakeInterbankTransfer()
         {
-            var interbankTransferDescription = (InterbankTransferDescription)command.TransferDescription;
+            var interbankTransferDescription = (InterbankTransferDescription)_transferDescription;
             _interbankTransferService.Transfer(interbankTransferDescription);
         }
 
-        private void UpdateAccountBalance(ExternalTransferCommand command)
+        private void UpdateAccountBalance()
         {
-            var transferDescription = command.TransferDescription;
+            Account = _dataContext.Accounts
+                .Single(account => account.Number == _transferDescription.From);
 
-            var senderAccount = _dataContext.Accounts
-                .Single(account => account.Number == transferDescription.From);
+            ChangeAccountBalance(_transferDescription.Amount);
 
-            senderAccount.Balance -= transferDescription.Amount;
             _dataContext.SaveChanges();
         }
     }
