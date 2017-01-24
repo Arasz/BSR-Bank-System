9413c9d3c1bc9d6bdb039102d10b3d549758c831
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-24 13:50:32 +0100

    Cretate and bind commands

--- Client.LightClient/ViewModel/AccountsViewModel.cs
+++ Client.LightClient/ViewModel/AccountsViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Data.Core;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using GalaSoft.MvvmLight.Views;
 using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace Client.LightClient.ViewModel
 {
@@ -12,6 +14,8 @@ namespace Client.LightClient.ViewModel
         private Account _selectedAccount;
         private string _username;
 
+        public ICommand AccountHistoryCommand { get; }
+
         public ICollection<Account> Accounts
         {
             get { return _accounts; }
@@ -19,6 +23,10 @@ namespace Client.LightClient.ViewModel
         }
 
         public string Amount { get; set; }
+
+        public ICommand DepositCommand { get; }
+
+        public ICommand ExternalTransferCommand { get; }
 
         public Account SelectedAccount
         {
@@ -28,16 +36,43 @@ namespace Client.LightClient.ViewModel
 
         public string TargetAccountNumber { get; set; }
 
+        public ICommand TransferCommand { get; }
+
         public string Username
         {
             get { return _username; }
             set { Set(ref _username, value); }
         }
 
+        public ICommand WithdrawCommand { get; }
+
         public AccountsViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+
             MessengerInstance.Register<User>(this, UserLogged);
+
+            DepositCommand = new RelayCommand(Deposit);
+            WithdrawCommand = new RelayCommand(Withdraw);
+            ExternalTransferCommand = new RelayCommand(MakeExternalTransfer);
+            TransferCommand = new RelayCommand(MakeTransfer);
+            AccountHistoryCommand = new RelayCommand(GetAccountHistory);
+        }
+
+        private void Deposit()
+        {
+        }
+
+        private void GetAccountHistory()
+        {
+        }
+
+        private void MakeExternalTransfer()
+        {
+        }
+
+        private void MakeTransfer()
+        {
         }
 
         private void UserLogged(User loggedUser)
@@ -45,6 +80,10 @@ namespace Client.LightClient.ViewModel
             Username = loggedUser.Name;
 
             Accounts = loggedUser.Accounts;
+        }
+
+        private void Withdraw()
+        {
         }
     }
 }
